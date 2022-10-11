33bb38a07c99e8196cc731a8e7a2c8232d0b86e9
AmayaB <bryanta21@nku.edu>
2022-10-11 16:26:29 -0400

    Added in quotes folder. Modified signin.php and signup.php by starting to validate login information. There is a reference I found for redirecting users to a different page at the top of signin.php

--- auth/signin.php
+++ auth/signin.php
@@ -1,28 +1,43 @@
 <?php
 session_start();
-// if the user is alreay signed in, redirect them to the members_page.php page
+// if the user is alreay signed in, redirect them to the members_page.php page: reference https://code.tutsplus.com/tutorials/how-to-redirect-with-php--cms-34680
 
 // use the following guidelines to create the function in auth.php
-//instead of using "die", return a message that can be printed in the HTML page
+
+$errorMessage =''; //instead of using "die", return a message that can be printed in the HTML page
+
 if(count($_POST)>0){
 	// 1. check if email and password have been submitted
-	// 2. check if the email is well formatted
-	// 3. check if the password is well formatted
-	// 4. check if the file containing banned users exists
-	// 5. check if the email has been banned
-	// 6. check if the file containing users exists
-	// 7. check if the email is registered
-	// 8. check if the password is correct
-	// 9. store session information
-	// 10. redirect the user to the members_page.php page
+	if (empty($_POST['username']) || empty($_POST['password']) ||) {
+    	// 2. check if the email is well formatted
+		if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+			// 3. check if the password is well formatted
+			if(!empty($_POST['password']) && $_POST['password'] != " "){
+			// 4. check if the file containing banned users exists
+			// 5. check if the email has been banned
+			// 6. check if the file containing users exists
+			// 7. check if the email is registered
+			// 8. check if the password is correct
+			// 9. store session information
+			// 10. redirect the user to the members_page.php page
+			
+			/*
+			echo 'check email+password';
+			if(true){
+				$_SESSION['logged']=true;
+				
+			}else $_SESSION['logged']=false;
+			*/
+			} else{
+				$errorMessage = 'Please enter your password';
+			}
+		} else{
+			$errorMessage = 'Your email is invalid';
+		}
+	} else {
+		$errorMessage = 'Please enter a username and password';
+	}
 	
-	/*
-	echo 'check email+password';
-	if(true){
-		$_SESSION['logged']=true;
-		
-	}else $_SESSION['logged']=false;
-	*/
 }
 
 // improve the form
@@ -30,6 +45,5 @@ if(count($_POST)>0){
 <form method="POST">
 	<input type="email" name="email" />
 	<input type="password" name="password" />
-	
-	<input type="submit" value="submit" />
+	<button type="submit">Log in</button>
 </form>
